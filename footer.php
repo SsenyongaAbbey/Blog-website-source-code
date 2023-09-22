92c7ce23eb1e93eeeb9722223ad0f41fb35e5ecb
Ssenyonga Abbey <61491363+SsenyongaAbbey@users.noreply.github.com>
2023-09-22 09:51:14 +0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,77 @@
+<!--footer-->
+	<footer style="background-color: maroon;">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-4 footer-grid-agileits-w3ls text-left">
+					<h3>About US</h3>
+<!-- 					<p>This website is about publishing articles</p>
+ -->					<div class="read">
+						<a style="background-color: grey;" href="about.php" class="btn btn-primary read-m">Read More</a>
+					</div>
+				</div>
+				<div class="col-lg-4 footer-grid-agileits-w3ls text-left">
+
+					<div class="tech-btm">
+						<h3>Popular Posts</h3>
+						<!-- <?php getpopularposts("page_hits"); ?> -->
+					</div>
+				</div>
+				<!-- subscribe -->
+				<div class="col-lg-4 subscribe-main footer-grid-agileits-w3ls text-left">
+					<h2>Signup to our newsArticle</h2>
+					<div class="subscribe-main text-left">
+							<div class="subscribe-form">
+									<form action="#" method="post" class="subscribe_form">
+										<input style="background-color: white; color: black" class="form-control" type="email" placeholder="Enter your email..." required="">
+										<button style="background-color: grey; color: white;" type="submit" class="btn btn-primary submit">Submit</button>
+									</form>
+									<div class="clearfix"> </div>
+						   </div>
+						<p>We respect your privacy</p>
+					</div>
+					<!-- //subscribe -->
+				</div>
+			</div>
+			<!-- footer -->
+			<div class="footer-cpy text-center">
+				<div class="footer-social">
+					<div class="copyrighttop">
+					<!-- 	<ul>
+							<li class="mx-3">
+								<a class="facebook" href="<?php getlinks("links","facebook");?>">
+									<i class="fab fa-facebook-f"></i>
+									<span>Facebook</span>
+								</a>
+							</li>
+							<li>
+								<a class="facebook" href="<?php getlinks("links","twitter");?>">
+									<i class="fab fa-twitter"></i>
+									<span>Twitter</span>
+								</a>
+							</li>
+							<li class="mx-3">
+								<a class="facebook" href="<?php getlinks("links","googleplus");?>">
+									<i class="fab fa-google-plus-g"></i>
+									<span>Google+</span>
+								</a>
+							</li>
+							<li>
+								<a class="facebook" href="<?php getlinks("links","pinterest");?>">
+									<i class="fab fa-pinterest-p"></i>
+									<span>Pinterest</span>
+								</a>
+							</li>
+						</ul>
+ -->
+					</div>
+				</div>
+				<div class="w3layouts-agile-copyrightbottom">
+					<!-- <p> Articles <?php $current=date("Y"); print_r($current);?> | Brought To You by
+						<a href="https://www.youtube.com/channel/UCsFgC9ggwrmYR2XqEHXpbNg/">Ser Bermz</a>
+					</p>
+ -->
+				</div>
+			</div>
+			<!-- //footer -->
+		</div>
+	</footer>
